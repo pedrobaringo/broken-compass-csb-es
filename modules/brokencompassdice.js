16d85cd38834428e8e99cfbe269fd2137866208f
pedrobaringo <pedro.baringo@gmail.com>
2024-10-04 20:13:48 +0200

    1.1.0

--- modules/brokencompassdice.js
+++ modules/brokencompassdice.js
@@ -5,8 +5,29 @@ Hooks.once("init", async function () {
 });
 
 Hooks.once('diceSoNiceReady', (dice3d) => {
-    dice3d.addSystem({id:"brokencompassblackDice",name:"Broken Compass Black Dice"},"default");
+dice3d.addSystem({id:"brokencompassblackDice",name:"Broken Compass Black Dice"},"default");
     dice3d.addDicePreset({
+      type:"dh",
+      labels:[
+        'modules/broken-compass-csb-es/assets/Cara1.webp', 
+        'modules/broken-compass-csb-es/assets/Cara2.webp', 
+        'modules/broken-compass-csb-es/assets/Cara3.webp', 
+        'modules/broken-compass-csb-es/assets/Cara4.webp',
+        'modules/broken-compass-csb-es/assets/Cara5.webp', 
+        'modules/broken-compass-csb-es/assets/Cara6.webp'
+      ],
+      bumpMaps:[
+        'modules/broken-compass-csb-es/assets/Cara1_Bump.webp', 
+        'modules/broken-compass-csb-es/assets/Cara2_Bump.webp', 
+        'modules/broken-compass-csb-es/assets/Cara3_Bump.webp', 
+        'modules/broken-compass-csb-es/assets/Cara4_Bump.webp',
+        'modules/broken-compass-csb-es/assets/Cara5_Bump.webp', 
+        'modules/broken-compass-csb-es/assets/Cara6_Bump.webp'
+      ],
+	  colorset:"blackBroken",
+      system: "brokencompassblackDice"
+    });
+	dice3d.addDicePreset({
       type:"d6",
       labels:[
         'modules/broken-compass-csb-es/assets/Cara1.webp', 
@@ -39,6 +60,27 @@ Hooks.once('diceSoNiceReady', (dice3d) => {
 });
 dice3d.addSystem({id:"brokencompassyellowDice",name:"Broken Compass Yellow Dice"},"preferred");
     dice3d.addDicePreset({
+      type:"dh",
+      labels:[
+        'modules/broken-compass-csb-es/assets/Cara1_Bump.webp', 
+        'modules/broken-compass-csb-es/assets/Cara2_Bump.webp', 
+        'modules/broken-compass-csb-es/assets/Cara3_Bump.webp', 
+        'modules/broken-compass-csb-es/assets/Cara4_Bump.webp',
+        'modules/broken-compass-csb-es/assets/Cara5_Bump.webp', 
+        'modules/broken-compass-csb-es/assets/Cara6_Bump.webp'
+      ],
+      bumpMaps:[
+        'modules/broken-compass-csb-es/assets/Cara1_Bump.webp', 
+        'modules/broken-compass-csb-es/assets/Cara2_Bump.webp', 
+        'modules/broken-compass-csb-es/assets/Cara3_Bump.webp', 
+        'modules/broken-compass-csb-es/assets/Cara4_Bump.webp',
+        'modules/broken-compass-csb-es/assets/Cara5_Bump.webp', 
+        'modules/broken-compass-csb-es/assets/Cara6_Bump.webp'
+      ],
+	  colorset:"yellowBroken",
+      system: "brokencompassyellowDice"
+    });
+	dice3d.addDicePreset({
       type:"d6",
       labels:[
         'modules/broken-compass-csb-es/assets/Cara1_Bump.webp', 
